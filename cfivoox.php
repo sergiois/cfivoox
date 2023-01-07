3cc406e8b027aba322e1e9fa16c096022d6da7fb
Sergio Iglesias (Dominio) <sergioi@socespcardio.cor>
2023-01-07 19:36:24 +0100

    Version 1.1.0: Joomla 4 compatibility

--- cfivoox.php
+++ cfivoox.php
@@ -3,16 +3,13 @@
  * @package     Joomla.Plugin
  * @subpackage  Fields.Ivoox
  *
- * @copyright   Copyright (C) 2019 SergioIglesiasNET
+ * @copyright   Copyright (C) 2023 SergioIglesiasNET
  * @license     GNU General Public License v2.0
  * @author 		Sergio Iglesias (@sergiois)
  */
 
- defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
-JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRATOR);
-
-
-class PlgFieldsCfivoox extends FieldsPlugin
+class PlgFieldsCfivoox extends \Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin
 {
 }
