fafb93835b105dec9c403f9d77e6da424f1258fb
McKillo <mckillo@hotmail.com>
2026-06-11 03:13:47 +0200

    Release v.2.0 for joomla 6

---
+++ src/Extension/Cfivoox.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.Cfivoox
+ */
+
+namespace SergioIglesias\Plugin\Fields\Cfivoox\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin;
+
+final class Cfivoox extends FieldsPlugin
+{
+}
+
